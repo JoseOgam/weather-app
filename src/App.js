0276c8b42ca1982d346c902f165d577561a47327
JoseOgam <jotienoochieng@gmail.com>
2020-08-08 10:22:18 -0400

    changing the landing page layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>React Weather App</h1>
       </header>
+      ...
+      <main>{/* add weather fetching component */}</main>
+      <footer>Page created by Joseph</footer>
+      ...
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
